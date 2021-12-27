1663406bc75fc0b4f49d02cdd4d01569219964e3
Uvindu1 <uvinducs153@gmail.com>
2021-12-27 22:34:28 +0530

    well come poster image

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import WellComeArea from "./components/WellComeArea";
-
-
-
-
+import WellComeImg from "./components/Well_Come_Area"
 function App() {
   return (
     <div>
       <WellComeArea/>
+      <WellComeImg/>
     </div>
   );
 }

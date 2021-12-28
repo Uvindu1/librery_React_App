247af1f80939266edc3097696ba78373dbd7bcc7
Uvindu1 <uvinducs153@gmail.com>
2021-12-28 14:06:25 +0530

    bottem side done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import WellComeArea from "./components/WellComeArea";
 import WellComeImg from "./components/Well_Come_Area";
 import PhotoCredit from "./components/photo_credit"
+import "../src/assets/styles/main.css";
 function App() {
   return (
     <div>
       <WellComeArea/>
       <WellComeImg/>
       <PhotoCredit/>
+      <div className='libArea'>
+        <div className='topArea'>
+          <h2>Books</h2>
+          <div className='line-2'></div>
+        </div>
+
+        <div className='topArea'>
+          <h2>Authors</h2>  
+          <div className='line-2'></div>             
+        </div>
+      </div>
     </div>
   );
 }

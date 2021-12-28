451514a3316e3d26e5c799496b767bd317c66147
Uvindu1 <uvinducs153@gmail.com>
2021-12-28 12:27:31 +0530

    done well come branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import WellComeArea from "./components/WellComeArea";
-import WellComeImg from "./components/Well_Come_Area"
+import WellComeImg from "./components/Well_Come_Area";
+import PhotoCredit from "./components/photo_credit"
 function App() {
   return (
     <div>
       <WellComeArea/>
       <WellComeImg/>
+      <PhotoCredit/>
     </div>
   );
 }
---
+++ src/components/photo_credit.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/styles/main.css";
+
+const PhotoCredit = () => {
+    return (
+        <div className="photoCredit">
+            <p>photo credits</p>
+        </div>
+    );
+}
+export default PhotoCredit;
